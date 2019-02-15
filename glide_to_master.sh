f6adb2d12378c49b1ca1efbb04c29b3881d8dd4c
ianchildress <ianchildress@gmail.com>
2019-02-15 10:29:03 -0800

    replaced branch dependency from 'develop' to 'master' in the glide.yaml file

--- glide_to_master.sh
+++ glide_to_master.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-BRANCH=$1
-FILE=glide.yaml
 
-# check if glide.yam. exists
-if [ ! -f $FILE ]; then
-  echo "$FILE not found."
-  exit
+FILE=glide.yaml
+BRANCH=$1
+
+# check if the branch argument was supplied
+if [[ ! $BRANCH ]]; then
+    echo "argument missing: branch"
+    echo "usage: glide_to_master <branch>"
+    echo "example: glide_to_master.sh develop"
+    exit 1
+fi
+
+# check if glide.yaml. exists
+if [[ ! -f $FILE ]]; then
+    echo "$FILE not found."
+    exit
 fi
 
 # checkout the specified branch
@@ -21,9 +30,9 @@ if ! git pull; then
 fi
 
 # check if we need to make any changes
-if ! grep -Fxq "$BRANCH" $FILE; then
-  echo "'$BRANCH' not found in $FILE. No work to do, exiting."
-  exit
+if ! grep "$BRANCH" $FILE; then
+    echo "'$BRANCH' not found in $FILE. No work to do, exiting."
+    exit
 fi
 
 # replace the specified branch with master
