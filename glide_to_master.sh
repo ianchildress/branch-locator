90d9788fb0918166222e7e1b2726bc326f50bef7
ianchildress <ianchildress@gmail.com>
2019-02-15 10:17:20 -0800

    gitflow setup

--- glide_to_master.sh
+++ glide_to_master.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 BRANCH=$1
-git checkout $1 && git pull
-echo "Changing all glide.yaml repositories from $1 to master."
-sed -i "s,$1,master,g" glide.yaml
+FILE=glide.yaml
+
+# check if glide.yam. exists
+if [ ! -f $FILE ]; then
+  echo "$FILE not found."
+  exit
+fi
+
+# checkout the specified branch
+if ! git checkout $BRANCH; then
+    echo "branch '$BRANCH' not found, exiting."
+    exit 1
+fi
+
+# pull latest
+if ! git pull; then
+    echo "failed to pull branch '$BRANCH', exiting."
+    exit 1
+fi
+
+# check if we need to make any changes
+if ! grep -Fxq "$BRANCH" $FILE; then
+  echo "'$BRANCH' not found in $FILE. No work to do, exiting."
+  exit
+fi
+
+# replace the specified branch with master
+echo "changing all glide.yaml repositories from $1 to master"
+sed -i "s,$BRANCH,master,g" glide.yaml
+
+# commit
 git add .
-git commit -m "Changing all glide.yaml repositories from $1 to master."
+git commit -m "replaced branch dependency from '$1' to 'master' in the glide.yaml file"
